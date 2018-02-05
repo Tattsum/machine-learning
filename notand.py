977603511616030bef660588035d210bada66cac
Tattsum <kurohari35@gmail.com>
2018-02-05 12:24:58 +0900

    NANDゲート

---
+++ notand.py
@@ -0,0 +1,19 @@
+# coding: UTF-8
+
+import numpy as np
+def NAND(x1,x2):
+    x = np.array([x1,x2])
+    w = np.array([-0.5,-0.5]) # 重みとバイアスだけが ANDと異なる．
+    b = 0.7
+
+    tmp = np.sum(w*x) + b
+
+    if tmp <= 0:
+        return 0
+    elif tmp > 0:
+        return 1
+
+print(NAND(0,0))
+print(NAND(1,0))
+print(NAND(0,1))
+print(NAND(1,1))
