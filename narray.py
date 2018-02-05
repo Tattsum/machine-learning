793e7c752679c3bd67e0d3fc93dbeb4d805afb60
Tattsum <kurohari35@gmail.com>
2018-02-05 18:52:40 +0900

    内積の例の追加

--- narray.py
+++ narray.py
@@ -22,3 +22,19 @@ B = np.array([[5,6],[7,8]])
 print(B.shape)
 
 print(np.dot(A,B)) # 内積の計算
+
+A = np.array([[1,2,3],[4,5,6]])
+print(A.shape)
+
+B = np.array([[1,2],[3,4],[5,6]])
+print(B.shape)
+
+print(np.dot(A,B)) # 内積の計算
+
+A = np.array([[1,2],[3,4],[5,6]])
+print(A.shape)
+
+B = np.array([7,8])
+print(B.shape)
+
+print(np.dot(A,B)) # 内積の計算
