36dcbc59d091c7392065459ad3ffd7e3a5f84c88
Tattsum <kurohari35@gmail.com>
2018-02-05 18:46:38 +0900

    コメントアウトを追加

--- narray.py
+++ narray.py
@@ -3,6 +3,12 @@
 import numpy as np
 A = np.array([1,2,3,4])
 print(A)
-print(np.ndim(A))
-print(A.shape)
+print(np.ndim(A)) # 配列の次元の取得
+print(A.shape) # 配列の形状はインスタンス変数から取得
 print(A.shape[0])
+
+B = np.array([[1,2],[3,4],[5,6]])
+print(B)
+print(np.ndim(B))
+print(B.shape)
+print(B.shape[0])
