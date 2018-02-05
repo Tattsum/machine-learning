69cddba20f0cd80459acbf1c56acc6088eb47c3a
Tattsum <kurohari35@gmail.com>
2018-02-05 18:18:36 +0900

    ステップ関数のグラフ

--- step.py
+++ step.py
@@ -1,13 +1,18 @@
 # coding: UTF-8
 
 import numpy as np
+import matplotlib.pylab as plt
 def step_function(x):
-    y = x > 0
-    return y.astype(np.int)
-
+    return np.array(x>0,dtype=np.int)
 
 x = np.array([-1.0,1.0,2.0])
 print(x)
 y = x > 0
 print(y)
 print(step_function(x))
+
+x = np.arange(-5.0,5.0,0.1)
+y = step_function(x)
+plt.plot(x,y)
+plt.ylim(-0.1,1.1) # y軸の範囲を指定
+plt.show()
