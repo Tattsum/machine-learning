9e00c1a54a906241db30884037de078e133281f3
Tattsum <kurohari35@gmail.com>
2018-02-06 12:39:33 +0900

    恒等関数

--- NN.py
+++ NN.py
@@ -30,3 +30,19 @@ print(B2.shape)
 
 A2 = np.dot(Z1,W2) + B2
 Z2 = sigmoid(A2)
+
+def identity_function(x):
+    return x
+
+W3 = np.array([[0.1,0.3],[0.2,0.4]])
+B3 = np.array([0.1,0.2])
+
+A3 = np.dot(Z2,W3) + B3
+Y = identity_function(A3)
+
+print(A2.shape)
+print(Z2.shape)
+print(W3.shape)
+print(B3.shape)
+print(A3.shape)
+print(Y.shape)
