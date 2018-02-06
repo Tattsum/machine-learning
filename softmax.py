62496bc2b75e906ab4796bcae09ae7159dfaa64a
Tattsum <kurohari35@gmail.com>
2018-02-06 13:57:15 +0900

    オーバーフロー

--- softmax.py
+++ softmax.py
@@ -17,3 +17,11 @@ def softmax(a):
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
+
+a = np.array([1010,1000,990])
+print(np.exp(a))
+
+c = np.max(a)
+print (a-c)
+
+print(np.exp(a-c)/np.sum(np.exp(a-c)))
