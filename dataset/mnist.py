b7df7c0d5cca3679c064a24b9890937d7a7d5aa5
Tattsum <kurohari35@gmail.com>
2018-02-07 11:08:20 +0900

    fix bug

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -13,9 +13,9 @@ import numpy as np
 
 url_base = 'http://yann.lecun.com/exdb/mnist/'
 key_file = {
-    'train_img':'train_images-idx3-ubyte.gz',
+    'train_img':'train-images-idx3-ubyte.gz',
     'train_label':'train-labels-idx1-ubyte.gz',
-    'test_img':'t10k_images-idx3-ubyte.gz',
+    'test_img':'t10k-images-idx3-ubyte.gz',
     'test_label':'t10k-labels-idx1-ubyte.gz'
 }
 
---
+++ mnist_dataset.py
@@ -0,0 +1,13 @@
+# coding: UTF-8
+
+import os, sys
+# sys.path.append(os.pardir) # 親ディレクトリ
+
+from dataset.mnist import load_mnist
+
+(x_train, t_train),(x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
