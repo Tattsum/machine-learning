e1117aa3fc7a5361c8838d090e1ec853dfff7e5d
Tattsum <kurohari35@gmail.com>
2018-02-08 15:09:02 +0900

    fix bug

--- nueralnet_mnist.py
+++ nueralnet_mnist.py
@@ -25,7 +25,7 @@ def init_network():
     return network
 
 def predict(network,x):
-    W1, w2, W3 = network['W1'], network['W2'], network['W3']
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
     a1 = np.dot(x, W1) + b1
