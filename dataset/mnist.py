b86311e54a818db530524f48f8744c90cb98e4e7
Tattsum <kurohari35@gmail.com>
2018-02-08 14:55:05 +0900

    fix bug

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -112,7 +112,7 @@ def load_mnist(normalize=True, flatten=True, one_hot_label=False):
         for key in ('train_img', 'test_img'):
             dataset[key] = dataset[key].astype(np.float32)
             dataset[key] /= 255.0
-            
+
     if one_hot_label:
         dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
         dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
@@ -121,7 +121,7 @@ def load_mnist(normalize=True, flatten=True, one_hot_label=False):
         for key in ('train_img','test_img'):
             dataset[key] = dataset[key].reshape(-1,1,28,28)
 
-    return (dataset['train_img'],dataset['train_label'],dataset['test_img'],dataset['test_label'])
+    return (dataset['train_img'],dataset['train_label']),(dataset['test_img'],dataset['test_label'])
 
 if __name__ == '__main__':
     init_mnist()
