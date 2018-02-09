857e7e87eec7db825a4a86105897ddeb1c8093f5
Tattsum <kurohari35@gmail.com>
2018-02-09 11:45:58 +0900

    affineレイヤ

--- common/layers.py
+++ common/layers.py
@@ -35,3 +35,32 @@ class Sigmoid:
         dx = dout * (1.0 - self.out) * self.out
 
         return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.W =W
+        self.b = b
+
+        self.x = None
+        self.original_x_shape = None
+        # 重み・バイアスパラメータの微分
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        # テンソル対応
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+
+        out = np.dot(self.x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す（テンソル対応）
+        return dx
